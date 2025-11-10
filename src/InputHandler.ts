b298a948de21a33ba73213e92afc24d3d3be0e7d
Musiquammation <bryanmegherbi@gmail.com>
2025-11-10 21:15:52 +0100

    push

---
+++ src/InputHandler.ts
@@ -0,0 +1,489 @@
+import type { Control } from "./Control";
+
+type Mode = "zqsd" | "wasd";
+
+class Keydown {
+	left = false;
+	right = false;
+	up = false;
+	down = false;
+	debug = false;
+	enter = false;
+}
+
+enum Action {
+	NONE,
+	DOWN,
+	UP,
+	DOWN_THEN_UP,
+	UP_THEN_DOWN,
+};
+
+class KeyboardCollector {
+	left = Action.NONE;
+	right = Action.NONE;
+	up = Action.NONE;
+	down = Action.NONE;
+	debug = Action.NONE;
+	enter = Action.NONE;
+}
+
+
+
+export class InputHandler {
+	static CONTROLS: Control[] = ["left", "right", "up", "down", "debug", "enter"];
+	static CONTROL_STACK_SIZE = 256;
+
+	private keyboardUsed = false;
+	private mobileUsed = false;
+
+	private collectedKeys: Record<Control, Action> = new KeyboardCollector();
+
+	private keysDown: Record<Control, boolean> = new Keydown();
+
+	private firstPress: Record<Control, boolean> = new Keydown();
+
+	private killedPress: Record<Control, boolean> = new Keydown();
+
+	private keyMap: Record<string, Control>;
+
+	gameFirstRecord = 0;
+	gameRecords: Uint32Array[] | null = null;
+	frameCount = 0;
+	recordCompletion = -1;
+	recordState: 'none' | 'record' | 'emulate' = 'none';
+
+	static KEYBOARDS: Record<Mode, Record<string, Control>> = {
+		zqsd: {
+			KeyZ: 'up',
+			KeyQ: 'left',
+			KeyS: 'down',
+			KeyD: 'right',
+			KeyP: 'debug',
+			Space: 'up',
+			ArrowUp: 'up',
+			ArrowLeft: 'left',
+			ArrowDown: 'down',
+			ArrowRight: 'right',
+			Enter: 'enter'
+		},
+
+		wasd: {
+			KeyW: 'up',
+			KeyA: 'left',
+			KeyS: 'down',
+			KeyD: 'right',
+			KeyP: 'debug',
+			Space: 'up',
+			ArrowUp: 'up',
+			ArrowLeft: 'left',
+			ArrowDown: 'down',
+			ArrowRight: 'right',
+			Enter: 'enter'
+		},
+	};
+
+
+	constructor(mode: Mode) {
+		this.keyMap = InputHandler.KEYBOARDS[mode];
+	}
+
+	private onKeydown = (event: Event) => {
+		const e = event as KeyboardEvent;
+		const control = this.keyMap[e.code];
+		if (control) {
+			switch (this.collectedKeys[control]) {
+			case Action.NONE:
+				this.collectedKeys[control] = Action.DOWN;
+				break;
+				
+			case Action.DOWN:
+				break;
+				
+			case Action.UP:
+				this.collectedKeys[control] = Action.UP_THEN_DOWN;
+				break;
+
+			case Action.DOWN_THEN_UP:
+				this.collectedKeys[control] = Action.UP_THEN_DOWN;
+				break;
+
+			case Action.UP_THEN_DOWN:
+				this.collectedKeys[control] = Action.UP_THEN_DOWN;
+				break;
+
+			}
+		}
+	}
+
+	private onKeyup = (event: Event) => {
+		const e = event as KeyboardEvent;
+		const control = this.keyMap[e.code];
+		if (control) {
+			switch (this.collectedKeys[control]) {
+			case Action.NONE:
+				this.collectedKeys[control] = Action.UP;
+				break;
+				
+			case Action.DOWN:
+				this.collectedKeys[control] = Action.DOWN_THEN_UP;
+				break;
+				
+			case Action.UP:
+				break;
+
+			case Action.DOWN_THEN_UP:
+				this.collectedKeys[control] = Action.DOWN_THEN_UP;
+				break;
+
+			case Action.UP_THEN_DOWN:
+				this.collectedKeys[control] = Action.DOWN_THEN_UP;
+				break;
+				
+			}
+		}
+	}
+
+
+
+	private onButtonTouchStart = (control: Control | 'special', element: HTMLElement) => {
+		element.classList.add("high");
+
+		if (control === 'special') {
+			return;
+		}
+
+		switch (this.collectedKeys[control]) {
+		case Action.NONE:
+			this.collectedKeys[control] = Action.DOWN;
+			break;
+			
+		case Action.DOWN:
+			break;
+			
+		case Action.UP:
+			this.collectedKeys[control] = Action.UP_THEN_DOWN;
+			break;
+
+		case Action.DOWN_THEN_UP:
+			this.collectedKeys[control] = Action.UP_THEN_DOWN;
+			break;
+
+		case Action.UP_THEN_DOWN:
+			this.collectedKeys[control] = Action.UP_THEN_DOWN;
+			break;
+
+		}
+	}
+
+	private onButtonTouchEnd = (control: Control | 'special', element: HTMLElement) => {
+		element.classList.remove("high");
+		if (control === 'special') {
+			document.getElementById("mobileEntry-specialContainer")?.classList.toggle('hidden');
+			return;
+		}
+
+		switch (this.collectedKeys[control]) {
+		case Action.NONE:
+			this.collectedKeys[control] = Action.UP;
+			break;
+			
+		case Action.DOWN:
+			this.collectedKeys[control] = Action.DOWN_THEN_UP;
+			break;
+			
+		case Action.UP:
+			break;
+
+		case Action.DOWN_THEN_UP:
+			this.collectedKeys[control] = Action.DOWN_THEN_UP;
+			break;
+
+		case Action.UP_THEN_DOWN:
+			this.collectedKeys[control] = Action.DOWN_THEN_UP;
+			break;
+			
+		}
+	}
+
+
+
+	startRecord() {
+		if (this.recordState === 'emulate')
+			return;
+
+		this.gameFirstRecord =
+			(+this.firstPress ['left']  <<  0) |
+			(+this.firstPress ['right'] <<  1) |
+			(+this.firstPress ['up']    <<  2) |
+			(+this.firstPress ['down']  <<  3) |
+			(+this.keysDown   ['left']  <<  4) |
+			(+this.keysDown   ['right'] <<  5) |
+			(+this.keysDown   ['up']    <<  6) |
+			(+this.keysDown   ['down']  <<  7) |
+			(+this.killedPress['left']  <<  8) |
+			(+this.killedPress['right'] <<  9) |
+			(+this.killedPress['up']    << 10) |
+			(+this.killedPress['down']  << 11);
+
+		
+
+		this.gameRecords = [];
+		this.recordCompletion = InputHandler.CONTROL_STACK_SIZE;
+		this.frameCount = 0;
+		this.recordState = 'record';
+	}
+
+	pushRecord() {
+		if (!this.gameRecords || this.recordState !== 'record')
+			return;
+
+		const line = (this.collectedKeys['left'] << 24) |
+			(this.collectedKeys['right'] << 16) |
+			(this.collectedKeys['up'] << 8) |
+			(this.collectedKeys['down']);
+
+
+		if (this.recordCompletion === InputHandler.CONTROL_STACK_SIZE) {
+			this.recordCompletion = 0;
+			this.gameRecords.push(new Uint32Array(InputHandler.CONTROL_STACK_SIZE));
+		}
+
+		this.gameRecords[this.gameRecords.length-1][this.recordCompletion] = line;
+		this.recordCompletion++;
+	}
+
+	stopRecord() {
+		this.recordState = 'none';
+	}
+
+	resumeRecord() {
+		if (this.recordState === 'none')
+			this.recordState = 'record';
+	}
+
+	async saveRecord() {
+		if (!this.gameRecords)
+			return;
+
+		
+
+		const fileHandle = await window.showSaveFilePicker!({
+			suggestedName: 'jumpyJump_record.bin',
+			types: [{
+				description: 'Binary data',
+				accept: { 'application/octet-stream': ['.bin'] },
+			}],
+		});
+
+
+		const writable = await fileHandle.createWritable();
+		await writable.write(new Uint32Array([this.gameFirstRecord]).slice(0));
+
+		for (let i = 0; i < this.gameRecords.length; i++) {
+			const arr = this.gameRecords[i];
+
+			if (i === this.gameRecords.length - 1 && this.recordCompletion !== undefined) {
+				const partial = arr.slice(0, this.recordCompletion);
+				await writable.write(partial);
+			} else {
+				await writable.write(arr.slice(0));
+			}
+		}
+
+		await writable.close();
+	}
+
+	async loadRecord() {
+		this.recordState = 'none';
+		this.frameCount = 1;
+		this.recordCompletion = 0;
+		
+		const [fileHandle] = await window.showOpenFilePicker!();
+		const file = await fileHandle.getFile();
+		const buffer = await file.arrayBuffer();
+		
+		this.recordState = 'emulate';
+		this.gameRecords = [new Uint32Array(buffer)];
+
+		const first = this.gameRecords[0][0];
+		function get(n: number) {return first & (1<<n) ? true : false}
+
+		this.firstPress['left'] = get(0);
+		this.firstPress['right'] = get(1);
+		this.firstPress['up'] = get(2);
+		this.firstPress['down'] = get(3);
+		this.keysDown['left'] = get(4);
+		this.keysDown['right'] = get(5);
+		this.keysDown['up'] = get(6);
+		this.keysDown['down'] = get(7);
+		this.killedPress['left'] = get(8);
+		this.killedPress['right'] = get(9);
+		this.killedPress['up'] = get(10);
+		this.killedPress['down'] = get(11);
+	}
+
+
+	restartRecord() {
+		this.startRecord();
+	}
+
+
+	startListeners(target: EventTarget) {
+		if ('ontouchstart' in window ||
+			navigator.maxTouchPoints > 0 ||
+			window.matchMedia("(pointer: coarse)").matches
+		) {
+			this.startMobileListeners();
+		} else {
+			this.enableKeyboardListeners(target);
+		}
+	}
+
+	removeListeners(target: EventTarget) {
+		if (this.keyboardUsed) {
+			target.removeEventListener("keydown", this.onKeydown);
+			target.removeEventListener("keyup", this.onKeyup);
+		}
+	}
+	
+	enableKeyboardListeners(target: EventTarget) {
+		target.addEventListener("keydown", this.onKeydown);
+		target.addEventListener("keyup", this.onKeyup);
+		this.keyboardUsed = true;
+	}
+	
+	startMobileListeners() {
+		const add = (id: string, control: Control | 'special') => {
+			const element = document.getElementById(id);
+			if (!element)
+				return;
+
+			element.ontouchstart = () => this.onButtonTouchStart(control, element);
+			element.ontouchend = () => this.onButtonTouchEnd(control, element);
+		}
+
+		document.getElementById("mobileEntryContainer")?.classList.remove("hidden");
+		
+		add("mobileEntry-left", 'left');
+		add("mobileEntry-right", 'right');
+		add("mobileEntry-up", 'up');
+		add("mobileEntry-down", 'down');
+		add("mobileEntry-special", 'special');
+		add("mobileEntry-special-enter", 'enter');
+		add("mobileEntry-special-debug", 'debug');
+		
+
+		this.mobileUsed = true;
+	}
+
+	update() {
+		if (this.recordState === 'record') {
+			this.pushRecord();
+		}
+
+		if (this.recordState === 'record' || this.recordState === 'none') {
+			for (const control of InputHandler.CONTROLS) {
+				this.play(control, this.collectedKeys[control]);
+				this.collectedKeys[control] = Action.NONE;
+			}
+		}
+
+		if (this.recordState === 'record') {
+			this.frameCount++;
+		
+		} else if (this.recordState === 'emulate') {
+			const line = this.gameRecords![0][this.frameCount];
+			this.play('left' , (line>>24)&0xff);
+			this.play('right', (line>>16)&0xff);
+			this.play('up',    (line>> 8)&0xff);
+			this.play('down',  (line>> 0)&0xff);
+			
+			this.frameCount++;
+		}
+			
+	}
+
+	play(control: Control, action: Action) {
+		switch (action) {
+		case Action.NONE:
+			this.firstPress[control] = false;
+			this.killedPress[control] = false;
+			break;
+
+		case Action.DOWN:
+			if (this.keysDown[control]) {
+				this.firstPress[control] = false;
+			} else {
+				this.firstPress[control] = true;
+				this.keysDown[control] = true;
+			}
+			this.killedPress[control] = false;
+			break;
+
+		case Action.UP:
+			if (this.keysDown[control]) {
+				this.firstPress[control] = false;
+				this.keysDown[control] = false;
+				this.killedPress[control] = true;
+			} else {
+				this.firstPress[control] = false;
+				this.killedPress[control] = false;
+			}
+			break;
+
+		case Action.DOWN_THEN_UP:
+			if (this.keysDown[control]) {
+				this.firstPress[control] = false;
+				this.keysDown[control] = false;
+			} else {
+				this.firstPress[control] = true;
+			}
+			this.killedPress[control] = true;
+
+			
+			break;
+
+		case Action.UP_THEN_DOWN:
+			if (this.keysDown[control]) {
+				this.firstPress[control] = false;
+				this.keysDown[control] = false;
+				this.killedPress[control] = true;
+			} else {
+				this.firstPress[control] = false;
+				this.killedPress[control] = false;
+			}
+
+			if (this.keysDown[control]) {
+				this.firstPress[control] = false;
+			} else {
+				this.firstPress[control] = true;
+				this.keysDown[control] = true;
+			}
+			this.killedPress[control] = false;
+			break;
+		}
+	}
+
+	press(control: Control): boolean {
+		return this.firstPress[control] || this.keysDown[control];
+	}
+
+	first(control: Control): boolean {
+		return this.firstPress[control];
+	}
+
+	killed(control: Control): boolean {
+		return this.killedPress[control];
+	}
+
+
+	kill(control: Control) {
+		this.keysDown[control] = false;
+	}
+
+
+	draw() {
+
+	}
+}
---
+++ src/importStage.ts
@@ -0,0 +1,298 @@
+import { bmodules, Block, BlockModule } from "./Block";
+import { Room } from "./Room";
+import { Stage } from "./Stage";
+
+const {
+    MovingModule,
+    CouldownedAttackModule,
+    ContinuousAttackModule,
+    BounceModule,
+    KillModule,
+    CouldownDespawnModule,
+    TouchDespawnModule,
+    HealModule,
+    SpeedModule,
+    AccelerationModule,
+    RestoreJumpModule,
+    RotationModule,
+    SpawnerModule,
+} = bmodules;
+
+
+export async function importStage(read: Function) {
+    const rooms: Room[] = [];
+
+    const roomBuffer = [0,0,0,0];
+    const blockBuffer = [0,0,0,0];
+    const moduleBuffer = [];
+
+    let currentMode: string | null = null;
+    let step = 0;
+    let blocks: Block[] = [];
+
+    class BlockModuleArg {
+        moving?: InstanceType<typeof MovingModule>;
+        rotation?: InstanceType<typeof RotationModule>;
+        couldownedAttack?: InstanceType<typeof CouldownedAttackModule>;
+        continuousAttack?: InstanceType<typeof ContinuousAttackModule>;
+        bounce?: InstanceType<typeof BounceModule>;
+        kill?: InstanceType<typeof KillModule>;
+        heal?: InstanceType<typeof HealModule>;
+        touchDespawn?: InstanceType<typeof TouchDespawnModule>;
+        restoreJump?: InstanceType<typeof RestoreJumpModule>;
+        couldownDespawn?: InstanceType<typeof CouldownDespawnModule>;
+        spawner?: InstanceType<typeof SpawnerModule>;
+        speed?: InstanceType<typeof SpeedModule>;
+        acceleration?: InstanceType<typeof AccelerationModule>;
+        
+        goal: number = 0;
+        checkCollision: boolean = false;
+        runInAdjacentRoom: boolean = false;
+    }
+
+    let blockToPush: BlockModuleArg | null = null;
+
+    
+
+    function take(word: string): number {
+        const num = Number(word);
+
+        if (!Number.isFinite(num)) {
+            throw new Error(`"${word}" is not a number`);
+        }
+
+        return num;
+    }
+
+    function pushBlock() {
+        if (!blockToPush)
+            return;
+    
+        blocks.push(new Block(
+            blockBuffer[0],
+            blockBuffer[1],
+            blockBuffer[2],
+            blockBuffer[3],
+            new BlockModule(blockToPush)
+        ))
+
+        blockBuffer[0] = -1;
+        blockBuffer[1] = -1;
+        blockBuffer[2] = -1;
+        blockBuffer[3] = -1;
+
+        blockToPush = null;
+    }
+
+    function pushRoom(forcePush = false) {
+        if (forcePush || blocks.length) {
+            console.log(roomBuffer[0],
+                roomBuffer[1],
+                roomBuffer[2],
+                roomBuffer[3]);
+
+            rooms.push(new Room(
+                roomBuffer[0],
+                roomBuffer[1],
+                roomBuffer[2],
+                roomBuffer[3],
+                blocks
+            ));
+
+            blocks = [];
+        }
+    }
+
+
+    for await (const word of read()) {
+        switch (currentMode) {
+        case "room":
+        {
+            pushBlock();
+
+            // generate last room
+            pushRoom();
+
+            roomBuffer[step] = take(word);
+            step++;
+
+            if (step === 4) {
+                currentMode = "block";
+                step = 0;
+            }
+            break;
+        }
+
+        case "emptyroom":
+        {
+            pushBlock();
+
+            // generate last room
+            pushRoom();
+            
+            roomBuffer[step] = take(word);
+            step++;
+
+            if (step === 4) {
+                currentMode = null;
+                step = 0;
+                pushRoom(true);
+            }
+            break;
+        }
+
+        case "block":
+        {
+            blockBuffer[step] = take(word);
+            step++;
+
+            if (step === 4) {
+                currentMode = null;
+                step = 0;
+                blockToPush = new BlockModuleArg();
+            }
+
+            break;
+        }
+
+        case "moving":
+            throw "Moving todo";
+            break;
+
+        case "rotation":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 2) { break; }
+            blockToPush!.rotation = new RotationModule(moduleBuffer[0], moduleBuffer[1]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "couldownedAttack":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 2) { break; }
+            blockToPush!.couldownedAttack = new CouldownedAttackModule(moduleBuffer[0], moduleBuffer[1]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "continuousAttack":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.continuousAttack = new ContinuousAttackModule(moduleBuffer[0]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "bounce":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 2) { break; }
+            blockToPush!.bounce = new BounceModule(moduleBuffer[0], moduleBuffer[1]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "kill":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.kill = new KillModule(!!(moduleBuffer[0]));
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "heal":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.heal = new HealModule(moduleBuffer[0]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "touchDespawn":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.touchDespawn = new TouchDespawnModule(!!(moduleBuffer[0]));
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "restoreJump":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.restoreJump = new RestoreJumpModule(moduleBuffer[0]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "couldownDespawn":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.couldownDespawn = new CouldownDespawnModule(moduleBuffer[0]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "spawner":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 3) { break; }
+            throw "Spawner todo";
+            // blockToPush!.spawner = new SpawnerModule(
+            // 	moduleBuffer[0],
+            // 	!!Number(moduleBuffer[1]),
+            // 	moduleBuffer[2]
+            // );
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "speed":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 2) { break; }
+            blockToPush!.speed = new SpeedModule(moduleBuffer[0], moduleBuffer[1]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "acceleration":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 2) { break; }
+            blockToPush!.acceleration = new AccelerationModule(moduleBuffer[0], moduleBuffer[1]);
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+        case "goal":
+            moduleBuffer.push(take(word));
+            if (moduleBuffer.length < 1) { break; }
+            blockToPush!.goal = moduleBuffer[0];
+            moduleBuffer.length = 0;
+            currentMode = null;
+            break;
+
+
+
+
+
+        case null:
+        {
+            const num = +word;
+            if (Number.isFinite(num)) {
+                pushBlock();
+
+                blockToPush = new BlockModuleArg();
+                blockBuffer[0] = take(word);
+                step = 1;
+                currentMode = "block";
+                
+            } else {
+                currentMode = word;
+            }
+            break;
+        }
+        
+        }
+    }
+
+    pushBlock();
+    pushRoom();
+    return new Stage(rooms);
+}
